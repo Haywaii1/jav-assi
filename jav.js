f3573122e8269f67c5d38589f744db9f7de3c3d2
haywaii1 <ayotundeonasanya@yahoo.com>
2022-05-20 12:48:51 +0100

    jscript progress

---
+++ jav.js
@@ -0,0 +1,83 @@
+//Design a loop that returns an array of even numbers from 1-100
+for (var even = 0; even < 100; even+=2){
+console.log(even);
+};
+
+//create a function that returns even number 
+function isEven(number){
+    if (Number %2 === 0){
+        return isEven;
+    } else {
+        console.log(number);
+    };
+} // besides i doubt any funtion can return even numbers between numbers.. it stop executing after ther first return
+
+
+//When would you use a conditioanl statemnet?
+
+when you want to loop through a group of statements
+
+// Create a function withh a function name firstfunction
+
+
+
+function firstFunction(a,b) {
+    let sum = a + b
+    return sum;
+}
+firstFunction(1,2);
+
+//Create a function with a function name printSum
+let num1 = 5;
+let num2 = 6;
+function printSum(num1,num2) { 
+    let add = num1 + num2 
+    console.log(num1 + num2);
+}
+printSum(5,6);
+
+//Create a function that returns an array of the names 10 of yoyr course mate
+
+let course = ["ayo", "tunde", "temi", "precious", "tobi", "emma", "tope", "seun", "ade", "sola"];
+
+function names() {
+    return[course];
+}
+names();
+
+//Answer the following:
+//Add element to the jend of the array
+let arr = [1,2,4];
+
+arr[3] = 6;
+console.log(arr);
+
+//Remove the last element in this array
+
+let rem = [1,2,3,8];
+
+rem.length = 3;
+console.log(rem);
+
+//Remove the first element in this array
+
+let fir = [23.77,80,45];
+fir[0] = 80;
+fir[1] = 45;
+fir.length = 2;
+console.log(fir);
+
+
+
+
+
+//Add an element to the beginning of the array
+
+let por = [[20,30,80],[10,50,55]];
+por[0] = [10,40,50];
+por[1] = [20,30,80];
+por[2] = [10,50,55]
+por.length = 3
+console.log(por);
+
+
